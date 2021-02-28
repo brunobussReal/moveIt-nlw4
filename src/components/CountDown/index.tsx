11414a399b27c3ce0355b50d6ff6d1ca85aca114
Bruno Buss <brunooliveira9895@gmail.com>
2021-02-28 13:03:10 +0000

    added countdown functionality

--- src/components/CountDown/index.tsx
+++ src/components/CountDown/index.tsx
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./CountDown.module.css";
 
 const CountDown: React.FC = () => {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  //convert minutes and seconds to string, if minutes has only 1 char add "0" at start and then split
+  const [minutesLeft, minutesRight] = String(minutes)
+    .padStart(2, "0")
+    .split("");
+  const [secondsLeft, secondsRight] = String(seconds)
+    .padStart(2, "0")
+    .split("");
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
   return (
-    <div className={styles.countDownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minutesLeft}</span>
+          <span>{minutesRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondsLeft}</span>
+          <span>{secondsRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>0</span>
-        <span>0</span>
-      </div>
+      <button onClick={startCountdown} className={styles.countdownButton}>
+        Start Cycle
+      </button>
     </div>
   );
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           {/* since this file loads only once, all fonts will be loaded once as well, optimazing the application */}
+          <link rel="shortcut icon" href="favicon.png" type="img/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
